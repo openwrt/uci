openwrt/uci

commit aa46546794acf5fb1f1f9bfb15152ea007e2c3c1

file: uci_file_commit: fix memory leak
Fixes following memory leak:

 26 bytes in 1 blocks are definitely lost in loss record 1 of 1
   at 0x4C2FB0F: malloc (in /usr/lib/valgrind/vgpreload_memcheck-amd64-linux.so)
   by 0x52DA68F: vasprintf (vasprintf.c:73)
   by 0x52B71D3: asprintf (asprintf.c:35)
   by 0x4E40F67: uci_file_commit (file.c:738)
   by 0x4E3FD94: uci_commit (libuci.c:193)
   by 0x401ED9: uci_do_import (cli.c:408)
   by 0x401ED9: uci_cmd (cli.c:685)
   by 0x4016FA: main (cli.c:776)

Signed-off-by: Petr Štetiar <ynezz@true.cz>

file.c
@@ -33,6 +33,10 @@
 #include "uci.h"
 #include "uci_internal.h"
 
+#ifndef MAX_PATH
+#define MAX_PATH 4096
+#endif
+
 #define LINEBUF	32
 
 /*
@@ -723,7 +727,7 @@ static void uci_file_commit(struct uci_context *ctx, struct uci_package **packag
 	FILE *f1, *f2 = NULL;
 	char *volatile name = NULL;
 	char *volatile path = NULL;
-	char *filename = NULL;
+	char filename[MAX_PATH] = {0};
 	struct stat statbuf;
 	volatile bool do_rename = false;
 	int fd;
@@ -735,7 +739,7 @@ static void uci_file_commit(struct uci_context *ctx, struct uci_package **packag
 			UCI_THROW(ctx, UCI_ERR_INVAL);
 	}
 
-	if ((asprintf(&filename, "%s/.%s.uci-XXXXXX", ctx->confdir, p->e.name) < 0) || !filename)
+	if (snprintf(filename, MAX_PATH, "%s/.%s.uci-XXXXXX", ctx->confdir, p->e.name) < 0)
 		UCI_THROW(ctx, UCI_ERR_MEM);
 
 	/* open the config file for writing now, so that it is locked */
@@ -808,7 +812,6 @@ done:
 		}
 		free(path);
 	}
-	free(filename);
 	if (ctx->err)
 		UCI_THROW(ctx, ctx->err);
 }

commit 19770b6949b9a95038f3216da773204eadfdd6bc

file: use dynamic memory allocation for tempfile name
Allocating a 4KB stack space buffer just for formatting a tempfile name
does not seem ideal.

Fixes: aa46546794ac ("file: uci_file_commit: fix memory leak")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>
Signed-off-by: Petr Štetiar <ynezz@true.cz>

file.c
@@ -33,10 +33,6 @@
 #include "uci.h"
 #include "uci_internal.h"
 
-#ifndef MAX_PATH
-#define MAX_PATH 4096
-#endif
-
 #define LINEBUF	32
 
 /*
@@ -727,10 +723,10 @@ static void uci_file_commit(struct uci_context *ctx, struct uci_package **packag
 	FILE *f1, *f2 = NULL;
 	char *volatile name = NULL;
 	char *volatile path = NULL;
-	char filename[MAX_PATH] = {0};
+	char *filename = NULL;
 	struct stat statbuf;
 	volatile bool do_rename = false;
-	int fd;
+	int fd, sz;
 
 	if (!p->path) {
 		if (overwrite)
@@ -739,8 +735,9 @@ static void uci_file_commit(struct uci_context *ctx, struct uci_package **packag
 			UCI_THROW(ctx, UCI_ERR_INVAL);
 	}
 
-	if (snprintf(filename, MAX_PATH, "%s/.%s.uci-XXXXXX", ctx->confdir, p->e.name) < 0)
-		UCI_THROW(ctx, UCI_ERR_MEM);
+	sz = snprintf(NULL, 0, "%s/.%s.uci-XXXXXX", ctx->confdir, p->e.name);
+	filename = alloca(sz + 1);
+	snprintf(filename, sz + 1, "%s/.%s.uci-XXXXXX", ctx->confdir, p->e.name);
 
 	/* open the config file for writing now, so that it is locked */
 	f1 = uci_open_stream(ctx, p->path, NULL, SEEK_SET, true, true);